a1bab5df356fac653f004d27b630c1495fd51a3c
Rafael Silva de Oliveira <rafasdoliveira@outlook.com>
2024-03-08 12:32:59 -0300

    Tela de contato

--- src/constants/navRedeSocial/navRedeSocial.jsx
+++ src/constants/navRedeSocial/navRedeSocial.jsx
@@ -2,22 +2,22 @@
 import { useState } from 'react';
 
 // Ícones das redes sociais
-import Instagram from '../../assets/images/instagram.svg';
-import LinkedIn from '../../assets/images/linkedin-in.svg';
-import GitHub from '../../assets/images/github.svg';
-import Unsplash from '../../assets/images/unsplash.svg';
-import Youtube from '../../assets/images/youtube.svg';
-import Tiktok from '../../assets/images/tiktok.svg';
+import Instagram from '../../assets/icons/redes-sociais/instagram.svg';
+import LinkedIn from '../../assets/icons/redes-sociais/linkedin-in.svg';
+import GitHub from '../../assets/icons/redes-sociais/github.svg';
+import Unsplash from '../../assets/icons/redes-sociais/unsplash.svg';
+import Youtube from '../../assets/icons/redes-sociais/youtube.svg';
+import Tiktok from '../../assets/icons/redes-sociais/tiktok.svg';
 
 const NavRedeSocial = () => {
 
     const [navSocial] = useState([
-        { id: 1, src: {Instagram}, alt: 'Instagram ícone', url: 'https://instagram.com/rafasdoliveira' },
-        { id: 2, src: {LinkedIn},  alt: 'LinkedIn ícone',  url: 'https://www.linkedin.com/in/rafasdoliveira/' },
-        { id: 3, src: {GitHub},    alt: 'GitHub ícone',    url: 'https://github.com/rafasdoliveira' },
-        { id: 4, src: {Unsplash},  alt: 'Unsplash ícone',  url: 'https://unsplash.com/pt-br/@rafasdoliveira' },
-        { id: 5, src: {Youtube},   alt: 'Youtube ícone',   url: '' },
-        { id: 6, src: {Tiktok},    alt: 'Tiktok ícone',    url: '' },
+        { id: 1, src: Instagram, alt: 'Instagram ícone', url: 'https://instagram.com/rafasdoliveira' },
+        { id: 2, src: LinkedIn,  alt: 'LinkedIn ícone',  url: 'https://www.linkedin.com/in/rafasdoliveira/' },
+        { id: 3, src: GitHub,    alt: 'GitHub ícone',    url: 'https://github.com/rafasdoliveira' },
+        { id: 4, src: Unsplash,  alt: 'Unsplash ícone',  url: 'https://unsplash.com/pt-br/@rafasdoliveira' },
+        { id: 5, src: Youtube,   alt: 'Youtube ícone',   url: '' },
+        { id: 6, src: Tiktok,    alt: 'Tiktok ícone',    url: '' },
     ])   
 
     return navSocial
--- src/pages/contato/contato.jsx
+++ src/pages/contato/contato.jsx
@@ -1,8 +1,44 @@
-const Blog = () => {
+// Componente
+import NavRedeSocial from '../../constants/navRedeSocial/navRedeSocial';
+// Rota
+import { Link } from 'react-router-dom';
+// Estilos
+import './contato.css';
+
+const Contato = () => {
+
+    const RedeSocial = NavRedeSocial() 
+
     return (
-        <div>
-        </div>
+        <>
+            <div className="contatoPage">
+                <div className="textos">
+                    <h1>Contato.</h1>
+                    <p>Fale comigo nas redes sociais ou por e-mail!</p>
+                </div>
+                <div className="icons">
+                    <ul className='ul-icons'>
+                        {RedeSocial.map((item) => (
+                            <li key={item.id}>
+                                <Link to={item.url}>
+                                    <img src={item.src} alt={item.alt}/>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="form">
+                    <form>
+                        <h1 className='textos'>Me envie um e-mail</h1>
+                        <input type="text" name="assunto" id="assunto" placeholder='Assunto'/>
+                        <input type="text" name="nome" id="nome" placeholder='Nome'/>
+                        <input type="email" name="email" id="email" placeholder='E-mail'/>
+                        <textarea name="mensagem" id="mensagem" placeholder='Mensagem'/>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
-export default Blog;
+export default Contato;
