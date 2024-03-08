1af2f110a8372b7c473f7ffb95c8270a483a742f
Rafael Silva de Oliveira <rafasdoliveira@outlook.com>
2024-03-08 13:02:28 -0300

    Layout Contato

--- src/pages/contato/contato.jsx
+++ src/pages/contato/contato.jsx
@@ -12,21 +12,7 @@ const Contato = () => {
     return (
         <>
             <div className="contatoPage">
-                <div className="textos">
-                    <h1>Contato.</h1>
-                    <p>Fale comigo nas redes sociais ou por e-mail!</p>
-                </div>
-                <div className="icons">
-                    <ul className='ul-icons'>
-                        {RedeSocial.map((item) => (
-                            <li key={item.id}>
-                                <Link to={item.url}>
-                                    <img src={item.src} alt={item.alt}/>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                
                 <div className="form">
                     <form>
                         <h1 className='textos'>Me envie um e-mail</h1>
@@ -34,7 +20,24 @@ const Contato = () => {
                         <input type="text" name="nome" id="nome" placeholder='Nome'/>
                         <input type="email" name="email" id="email" placeholder='E-mail'/>
                         <textarea name="mensagem" id="mensagem" placeholder='Mensagem'/>
+                        <input type="submit" value="Enviar" />
                     </form>
+                </div>
+                <div className="redes-sociais">
+                <div className="textos">
+                        <p>Fale nas redes sociais ou por e-mail!</p>
+                    </div>
+                    <div className="icons">
+                        <ul className='ul-icons'>
+                            {RedeSocial.map((item) => (
+                                <li key={item.id}>
+                                    <Link to={item.url}>
+                                        <img src={item.src} alt={item.alt}/>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>   
                 </div>
             </div>
         </>
