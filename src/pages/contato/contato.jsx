388bbd32166db5d7676db3df6cacb54f7321c6e0
Rafael Silva de Oliveira <rafasdoliveira@outlook.com>
2024-03-26 12:17:29 -0300

    Formulario HTTP

--- src/pages/contato/contato.jsx
+++ src/pages/contato/contato.jsx
@@ -5,10 +5,66 @@ import NavRedeSocial from '../../constants/navRedeSocial/navRedeSocial';
 import Header from '../../components/header/header';
 // Estilos
 import './contato.css';
+import { useState, useEffect } from 'react';
+
+const url = 'http://localhost:3000/forms'
 
 const Contato = () => {
 
     const RedeSocial = NavRedeSocial() 
+    const [forms, setForms] = useState([])
+    const [assunto, setAssunto] = useState('')
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [telefone, setTelefone] = useState('')
+    const [mensagem, setMensagem] = useState('')
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url)
+                if(!response) {
+                    throw new Error('Erro ao buscar os dados')
+                }
+                const data = await response.json()
+                setForms(data)
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
+
+    console.log(forms)
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const form = {
+            assunto,
+            nome,
+            email,
+            telefone,
+            mensagem
+        }
+
+        try{
+            const response = await fetch(url, {
+                method: 'POST', 
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form)
+            })
+            if(!response.ok) {
+                throw new Error('Erro ao enviar os dados')
+            }
+            } catch (error) {
+                alert(error)
+            }
+        }
 
     return (
         <>
@@ -24,13 +80,17 @@ const Contato = () => {
                 </div>
                 <form className='form'>
                     <div className="input">
-                        <input type="text" name="assunto" id="assunto" placeholder='Assunto'/>
-                        <input type="text" name="nome" id="nome" placeholder='Nome'/>
-                        <input type="email" name="email" id="email" placeholder='E-mail'/>
+                        <input type="text" value={assunto} name="assunto" id="assunto" placeholder='Assunto'onChange={(e) => setAssunto(e.target.value)}/>
+
+                        <input type="text" value={nome} name="nome" id="nome" placeholder='Nome' onChange={(e) => setNome(e.target.value)}/>
+
+                        <input type="email" value={email} name="email" id="email" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)}/>
+
+                        <input type="number" value={telefone} name="telefone" id="telefone" placeholder='Telefone' onChange={(e) => setTelefone(e.target.value)}/>
                         </div>
                     <div className="textarea">
-                        <textarea name="mensagem" id="mensagem" placeholder='Mensagem'/>
-                        <input type="submit" value="Enviar" />
+                        <textarea value={mensagem} name="mensagem" id="mensagem" placeholder='Mensagem' onChange={(e) => setMensagem(e.target.value)}/>
+                        <input type="submit" value="Enviar" onClick={handleSubmit} />
                     </div>
                 </form>
                 <div className="redes-sociais">
